5dba93bf067a6dfb7c78cc48525829eb249e3a75
jprasad2 <147555000+jprasad2@users.noreply.github.com>
2023-12-13 18:33:30 -0500

    login functionality, searchByUser function, template for validation

--- Project/validation.js
+++ Project/validation.js
@@ -1,5 +1,28 @@
 //validation functions
 
-export default {
+function checkName(str) {
     
+    return str
+}
+
+function checkEmail(str) {
+
+    return str
+}
+
+function checkUsername(str) {
+
+    return str
+}
+
+function checkPassword(str) {
+
+    return str
+}
+
+export default {
+    checkName,
+    checkEmail,
+    checkUsername,
+    checkPassword
 }
