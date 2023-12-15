d3c86adfad165a75af8eec0ee63e3c29e11cfbeb
michaeljhurtado <mhurtad1@stevens.edu>
2023-12-15 16:08:54 -0500

    html signup, basic clientValidation, commented out user/signup code temporarily

--- Project/public/js/clientValidation.js
+++ Project/public/js/clientValidation.js
@@ -1,71 +1,109 @@
+const loginform = document.getElementById("login-form");
+const searchform = document.getElementById("search-form");
 
-const loginform = document.getElementById("login-form")
-const searchform = document.getElementById("search-form")
+const signupform = document.getElementById("signup-form");
 
+//login inputs
+const emailAddressInput = document.getElementById("emailAddressInput");
+const passwordInput = document.getElementById("passwordInput");
+//search input
+const searchInput = document.getElementById("searchInput");
 
-const emailAddressInput = document.getElementById("emailAddressInput")
-const passwordInput = document.getElementById("passwordInput")
-
-const searchInput = document.getElementById("searchInput")
+//signup inputs
+const firstNameInput = document.getElementById("firstNameInput");
+const lastNameInput = document.getElementById("lastNameInput");
+const dateInput = document.getElementById("dateInput");
+const createEmailInput = document.getElementById("createEmailAddressInput");
+const createPasswordInput = document.getElementById("createPasswordInput");
+const confirmPasswordInput = document.getElementById("confirmPasswordInput");
+const usernameInput = document.getElementById("usernameInput");
 
 if (loginform) {
-    loginform.addEventListener('submit', (event) => {
-        try {
-            checkEmail(emailAddressInput.value)
-            checkPassword(passwordInput.value)
-            //error.hidden = true
-        } catch (e) {
-            event.preventDefault()
-            error.innerHTML = e
-        }
-    })
+  loginform.addEventListener("submit", (event) => {
+    try {
+      checkEmail(emailAddressInput.value);
+      checkPassword(passwordInput.value);
+      //error.hidden = true
+    } catch (e) {
+      event.preventDefault();
+      error.innerHTML = e;
+    }
+  });
 }
 
 if (searchform) {
-    searchform.addEventListener('submit', (event) => {
-        try {
-            /*
+  searchform.addEventListener("submit", (event) => {
+    try {
+      /*
             There is no client-side validation for this event
             Users can enter an empty string and it will just search for all users
             Search function is a partial match
             */
-        } catch (e) {
-            event.preventDefault()
-            error.innerHTML = e
-        }
-    })
+    } catch (e) {
+      event.preventDefault();
+      error.innerHTML = e;
+    }
+  });
 }
 
-
+if (signupform) {
+  signupform.addEventListener("submit", (event) => {
+    try {
+      checkStr(firstNameInput);
+      checkStr(lastNameInput);
+      checkStr(createEmailInput);
+      checkStr(usernameInput);
+      checkStr(createPasswordInput);
+      checkStr(confirmPasswordInput);
+      checkPassword(createPasswordInput);
+      checkDate(dateInput);
+      checkUsername(usernameInput);
+      if (createPasswordInput !== confirmPasswordInput)
+        throw "Error: passwords do not match";
+    } catch (e) {
+      event.preventDefault();
+      console.log("made it here");
+      error.innerHTML = e;
+    }
+  });
+}
 
 function checkStr(str) {
-    if (!str) throw 'Error: please enter text'
-    if (typeof str != 'string') throw "error: input must be a string"
-    str = str.trim()
-    if (str.length == 0) throw "cannot enter nothing or empty spaces"
+  if (!str) throw "Error: please enter text";
+  if (typeof str != "string") throw "error: input must be a string";
+  str = str.trim();
+  if (str.length == 0) throw "cannot enter nothing or empty spaces";
+}
+
+function checkDate(date) {
+  console.log(date);
+  //complete this functionality
+}
+function checkUsername(username) {
+  console.log(username);
+  //complete this functionalty
 }
 
 function checkEmail(email) {
-    if (!email) throw 'Error: please enter an email'
-    if (typeof email != "string") throw "Error: email must be a string"
-    email = email.trim()
-    let emailpattern = /[\w-_.]+[\w]+\@[\w+-]+\.[\w]+[\w]+/g
-    let emailmatch = email.match(emailpattern)
-    if (emailmatch == null)
-          throw "invalid email"
+  if (!email) throw "Error: please enter an email";
+  if (typeof email != "string") throw "Error: email must be a string";
+  email = email.trim();
+  let emailpattern = /[\w-_.]+[\w]+\@[\w+-]+\.[\w]+[\w]+/g;
+  let emailmatch = email.match(emailpattern);
+  if (emailmatch == null) throw "invalid email";
 }
 
 function checkPassword(password) {
-    if (!password) throw 'Error: please enter a password'
-    if (typeof password != "string") throw 'Error: password must be a string'
-    password = password.trim()
+  if (!password) throw "Error: please enter a password";
+  if (typeof password != "string") throw "Error: password must be a string";
+  password = password.trim();
 
-    //at least one uppercase
-    //at least one number
-    //at least one special character
-    let regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]).{8,}$/g
-    //https://stackoverflow.com/a/72686232
-    let passmatch = password.match(regex)
-    if (passmatch == null)
-        throw "Password must contain at least one uppercase, number, and special character and be at least 8 characters"
+  //at least one uppercase
+  //at least one number
+  //at least one special character
+  let regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]).{8,}$/g;
+  //https://stackoverflow.com/a/72686232
+  let passmatch = password.match(regex);
+  if (passmatch == null)
+    throw "Password must contain at least one uppercase, number, and special character and be at least 8 characters";
 }
--- Project/routes/users.js
+++ Project/routes/users.js
@@ -1,40 +1,39 @@
-import {Router} from "express"
-const router = Router()
+import { Router } from "express";
+const router = Router();
 
-import validation from "../validation.js"
-import {userData} from "../data/index.js"
-import { portfolioData } from "../data/index.js"
+import validation from "../validation.js";
+import { userData } from "../data/index.js";
+import { portfolioData } from "../data/index.js";
 
-router
-    .route('/profile')
-    .get(async (req, res) => {
-        //console.log(req.session.user)
-        let portids = req.session.user.portfolioIDs
-        let ports = []
-        for (let i = 0; i < portids.length; i++)
-        {
-            ports.push(await portfolioData.getPortfolioById(portids[i]))
-        }
-        //console.log(ports)
-        res.render("./users/profile", {title: "Profile", User: req.session.user, Portfolio: ports})
-    })
-
-router
-    .route('/editprofile')
-    .get(async (req, res) => {
-        res.render("./users/editprofile", {title: "Edit Profile"})
-    })
-    .patch(async (req, res) => {
-        
-    })
+router.route("/profile").get(async (req, res) => {
+  //console.log(req.session.user)
+  let portids = req.session.user.portfolioIDs;
+  let ports = [];
+  for (let i = 0; i < portids.length; i++) {
+    ports.push(await portfolioData.getPortfolioById(portids[i]));
+  }
+  //console.log(ports)
+  res.render("./users/profile", {
+    title: "Profile",
+    User: req.session.user,
+    Portfolio: ports,
+  });
+});
 
 router
-    .route('/signup')
-    .get(async (req, res) => {
-        res.render("./users/signup", {title: "Signup"})
-    })
-    .post(async(req, res) => {
-        /*
+  .route("/editprofile")
+  .get(async (req, res) => {
+    res.render("./users/editprofile", { title: "Edit Profile" });
+  })
+  .patch(async (req, res) => {});
+
+router
+  .route("/signup")
+  .get(async (req, res) => {
+    res.render("./users/signup", { title: "Signup" });
+  })
+  .post(async (req, res) => {
+    /*
         request body:
         {
             firstName: "first name"
@@ -45,6 +44,7 @@ router
             password: "password"
         }
         */
+    /*
        try {
         let firstName = validation.checkName(req.body.username)
         let lastName = validation.checkName(req.body.password)
@@ -58,40 +58,42 @@ router
         console.log(e)
         res.status(400).json({error: e})
        }
-    })
+       */
+  });
 
 router
-    .route('/login')
-    .get((req, res) => {
-        res.render("./users/login", {title: "Login"})
-    })
-    .post(async(req, res) => {
-        req.body.emailAddressInput = validation.checkEmail(req.body.emailAddressInput)
-        req.body.passwordInput = validation.checkPassword(req.body.passwordInput)  
-        try {
-            let userInfo = await userData.loginUser(req.body.emailAddressInput, req.body.passwordInput)
-            req.session.name = "AuthState"
-            req.session.user = userInfo
-            res.redirect("../feed") 
-        } catch (e) {
-          console.log(e)
-          return res.status(400).render("./users/login", {
-            title: "Login",
-            error: e
-          })
-        }
-    })
+  .route("/login")
+  .get((req, res) => {
+    res.render("./users/login", { title: "Login" });
+  })
+  .post(async (req, res) => {
+    req.body.emailAddressInput = validation.checkEmail(
+      req.body.emailAddressInput
+    );
+    req.body.passwordInput = validation.checkPassword(req.body.passwordInput);
+    try {
+      let userInfo = await userData.loginUser(
+        req.body.emailAddressInput,
+        req.body.passwordInput
+      );
+      req.session.name = "AuthState";
+      req.session.user = userInfo;
+      res.redirect("../feed");
+    } catch (e) {
+      console.log(e);
+      return res.status(400).render("./users/login", {
+        title: "Login",
+        error: e,
+      });
+    }
+  });
 
-router
-    .route('/createport')
-    .get((req, res) => {
-        res.render("./users/createport", {title: "Create Portfolio"})
-    })
+router.route("/createport").get((req, res) => {
+  res.render("./users/createport", { title: "Create Portfolio" });
+});
 
-router
-    .route('/logout')
-    .get(async(req, res) => {
-        res.render("./users/logout", {title: "Logout"})
-    })
+router.route("/logout").get(async (req, res) => {
+  res.render("./users/logout", { title: "Logout" });
+});
 
-export default router
+export default router;
